87609f6a0b116ddb2519edb31a86aea67f303fb6
RogerTaule <roger.taulee@gmail.com>
2022-07-25 21:00:18 +0200

    week 3 assignment completed

--- Part1/contracts/circuits/MastermindVariation.circom
+++ Part1/contracts/circuits/MastermindVariation.circom
@@ -1,9 +1,100 @@
-pragma circom 2.0.0;
+pragma circom 2.0.3;
 
-// [assignment] implement a variation of mastermind from https://en.wikipedia.org/wiki/Mastermind_(board_game)#Variation as a circuit
+include "../../node_modules/circomlib/circuits/comparators.circom";
+include "../../node_modules/circomlib/circuits/bitify.circom";
+include "../../node_modules/circomlib/circuits/poseidon.circom";
 
 template MastermindVariation() {
+   // Public inputs
+    signal input pubGuess1;
+    signal input pubGuess2;
+    signal input pubGuess3;
 
-}
+    signal input pubNumHit;
+    signal input pubNumBlow;
+    signal input pubSolnHash;
 
-component main = MastermindVariation();
+    // Private inputs
+    signal input privSoln1;
+    signal input privSoln2;
+    signal input privSoln3;
+    signal input privSalt;
+
+    // Output
+    signal output solnHashOut;
+
+    var guess[3] = [pubGuess1, pubGuess2, pubGuess3];
+    var soln[3] =  [privSoln1, privSoln2, privSoln3];
+    var j = 0;
+    var k = 0;
+    component lessThan[6];
+    component equalGuess[8];
+    component equalSoln[8];
+    var equalIdx = 0;
+
+    // Create a constraint that the solution and guess digits are all less than 10.
+    for (j=0; j<3; j++) {
+        lessThan[j] = LessThan(5);
+        lessThan[j].in[0] <== guess[j];
+        lessThan[j].in[1] <== 25;
+        lessThan[j].out === 1;
+        lessThan[j+3] = LessThan(5);
+        lessThan[j+3].in[0] <== soln[j];
+        lessThan[j+3].in[1] <== 25;
+        lessThan[j+3].out === 1;
+        for (k=j+1; k<3; k++) {
+            // Create a constraint that the solution and guess digits are unique. no duplication.
+            equalGuess[equalIdx] = IsEqual();
+            equalGuess[equalIdx].in[0] <== guess[j];
+            equalGuess[equalIdx].in[1] <== guess[k];
+            equalGuess[equalIdx].out === 0;
+            equalSoln[equalIdx] = IsEqual();
+            equalSoln[equalIdx].in[0] <== soln[j];
+            equalSoln[equalIdx].in[1] <== soln[k];
+            equalSoln[equalIdx].out === 0;
+            equalIdx += 1;
+        }
+    }
+
+    // Count hit & blow
+    var hit = 0;
+    var blow = 0;
+    component equalHB[9];
+
+    for (j=0; j<3; j++) {
+        for (k=0; k<3; k++) {
+            equalHB[3*j+k] = IsEqual();
+            equalHB[3*j+k].in[0] <== soln[j];
+            equalHB[3*j+k].in[1] <== guess[k];
+            blow += equalHB[3*j+k].out;
+            if (j == k) {
+                hit += equalHB[3*j+k].out;
+                blow -= equalHB[3*j+k].out;
+            }
+        }
+    }
+
+    // Create a constraint around the number of hit
+    component equalHit = IsEqual();
+    equalHit.in[0] <== pubNumHit;
+    equalHit.in[1] <== hit;
+    equalHit.out === 1;
+    
+    // Create a constraint around the number of blow
+    component equalBlow = IsEqual();
+    equalBlow.in[0] <== pubNumBlow;
+    equalBlow.in[1] <== blow;
+    equalBlow.out === 1;
+
+    // Verify that the hash of the private solution matches pubSolnHash
+    component poseidon = Poseidon(4);
+    poseidon.inputs[0] <== privSalt;
+    poseidon.inputs[1] <== privSoln1;
+    poseidon.inputs[2] <== privSoln2;
+    poseidon.inputs[3] <== privSoln3;
+
+    solnHashOut <== poseidon.out;
+    pubSolnHash === solnHashOut;
+ }
+
+ component main {public [pubGuess1, pubGuess2, pubGuess3, pubNumHit, pubNumBlow, pubSolnHash]} = MastermindVariation();
--- Part1/test/mastermind-test.js
+++ Part1/test/mastermind-test.js
@@ -1,1 +1,190 @@
 //[assignment] write your own unit test to show that your Mastermind variation circuit is working as expected
+
+const { expect, assert } = require("chai");
+
+const wasm_tester = require("circom_tester").wasm;
+
+const F1Field = require("ffjavascript").F1Field;
+const Scalar = require("ffjavascript").Scalar;
+exports.p = Scalar.fromString("21888242871839275222246405745257275088548364400416034343698204186575808495617");
+const Fr = new F1Field(exports.p);
+const { BigNumber } = require("ethers");
+const { buildPoseidon } = require("circomlibjs");
+
+
+
+describe("MastermindVariation", function () {
+    this.timeout(100000000);
+    let Verifier;
+    let verifier;
+    let poseidon;
+    let circuit;
+
+    beforeEach(async function () {
+        Verifier = await ethers.getContractFactory("Verifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
+
+        circuit = await wasm_tester("contracts/circuits/MastermindVariation.circom");
+        poseidon =  await buildPoseidon();
+    });
+
+    it("Circuit returns correct number of hit and blow", async function () {
+       
+        const guess = [444, 0, 23, 9];
+        const hash = BigNumber.from(poseidon.F.toObject(poseidon(guess)));
+        
+        const INPUT = {
+            "pubSolnHash": hash,
+            "pubGuess1": 23,
+            "pubGuess2": 7,
+            "pubGuess3": 9,
+            "pubNumHit": 1,
+            "pubNumBlow": 1,
+            "privSoln1": 0,
+            "privSoln2": 23,
+            "privSoln3": 9,
+            "privSalt": 444
+        }
+
+        const witness = await circuit.calculateWitness(INPUT, true);
+
+        assert(Fr.eq(Fr.e(witness[0]),Fr.e(1)));
+        assert(Fr.eq(Fr.e(witness[1]),Fr.e(5004980421847786928530313861547727672039001312552586098193283482939648079471n)));
+
+    });
+
+    it("Circuit to be reverted if number of hits doesn't match  ", async function () {
+
+        const guess = [444, 0, 23, 9];
+        const hash = BigNumber.from(poseidon.F.toObject(poseidon(guess)));
+
+        const INPUT = {
+            "pubSolnHash": hash,
+            "pubGuess1": 23,
+            "pubGuess2": 7,
+            "pubGuess3": 9,
+            "pubNumHit": 0,
+            "pubNumBlow": 1,
+            "privSoln1": 0,
+            "privSoln2": 23,
+            "privSoln3": 9,
+            "privSalt": 444
+        }
+
+        expect(circuit.calculateWitness(INPUT, true)).to.be.reverted;
+
+    });
+
+    it("Circuit to be reverted if number of blows doesn't match  ", async function () {
+
+        const guess = [444, 0, 23, 9];
+        const hash = BigNumber.from(poseidon.F.toObject(poseidon(guess)));
+
+        const INPUT = {
+            "pubSolnHash": hash,
+            "pubGuess1": 23,
+            "pubGuess2": 7,
+            "pubGuess3": 9,
+            "pubNumHit": 1,
+            "pubNumBlow": 0,
+            "privSoln1": 0,
+            "privSoln2": 23,
+            "privSoln3": 9,
+            "privSalt": 444
+        }
+
+        expect(circuit.calculateWitness(INPUT, true)).to.be.reverted;
+
+    });
+
+    it("Circuit to be reverted if number is higher than 25 in guess", async function () {
+
+        const guess = [444, 0, 25, 9];
+        const hash = BigNumber.from(poseidon.F.toObject(poseidon(guess)));
+
+        const INPUT = {
+            "pubSolnHash": hash,
+            "pubGuess1": 23,
+            "pubGuess2": 7,
+            "pubGuess3": 9,
+            "pubNumHit": 1,
+            "pubNumBlow": 1,
+            "privSoln1": 0,
+            "privSoln2": 25,
+            "privSoln3": 9,
+            "privSalt": 444
+        }
+
+        expect(circuit.calculateWitness(INPUT, true)).to.be.reverted;
+
+    });
+
+    it("Circuit to be reverted if number is higher than 25 in solution", async function () {
+
+        const guess = [444, 0, 25, 9];
+        const hash = BigNumber.from(poseidon.F.toObject(poseidon(guess)));
+
+        const INPUT = {
+            "pubSolnHash": hash,
+            "pubGuess1": 27,
+            "pubGuess2": 7,
+            "pubGuess3": 9,
+            "pubNumHit": 1,
+            "pubNumBlow": 1,
+            "privSoln1": 0,
+            "privSoln2": 25,
+            "privSoln3": 9,
+            "privSalt": 444
+        }
+
+        expect(circuit.calculateWitness(INPUT, true)).to.be.reverted;
+
+    });
+
+    it("Circuit to be reverted if number is duplicated in guess", async function () {
+
+        const guess = [444, 25, 25, 9];
+        const hash = BigNumber.from(poseidon.F.toObject(poseidon(guess)));
+
+        const INPUT = {
+            "pubSolnHash": hash,
+            "pubGuess1": 23,
+            "pubGuess2": 7,
+            "pubGuess3": 9,
+            "pubNumHit": 1,
+            "pubNumBlow": 1,
+            "privSoln1": 25,
+            "privSoln2": 25,
+            "privSoln3": 9,
+            "privSalt": 444
+        }
+
+        expect(circuit.calculateWitness(INPUT, true)).to.be.reverted;
+
+    });
+
+    it("Circuit to be reverted if number is duplicated in solution", async function () {
+
+        const guess = [444, 25, 25, 9];
+        const hash = BigNumber.from(poseidon.F.toObject(poseidon(guess)));
+
+        const INPUT = {
+            "pubSolnHash": hash,
+            "pubGuess1": 23,
+            "pubGuess2": 9,
+            "pubGuess3": 9,
+            "pubNumHit": 1,
+            "pubNumBlow": 1,
+            "privSoln1": 23,
+            "privSoln2": 25,
+            "privSoln3": 9,
+            "privSalt": 444
+        }
+
+        expect(circuit.calculateWitness(INPUT, true)).to.be.reverted;
+
+    });
+
+    
+})
